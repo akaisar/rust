3aa844c47993eba8f6830c0362d0cf683a6df4f9
Altynbek <kajsar.alt@gmail.com>
2021-12-26 05:31:46 +0600

    GUESS-1: Add error handling

---
+++ guessing_game/target/debug/deps/guessing_game-1f016be660ee388e.d
@@ -0,0 +1,5 @@
+/Users/iplayer/Projects/rust/guessing_game/target/debug/deps/guessing_game-1f016be660ee388e: src/main.rs
+
+/Users/iplayer/Projects/rust/guessing_game/target/debug/deps/guessing_game-1f016be660ee388e.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/iplayer/Projects/rust/guessing_game/target/debug/guessing_game: /Users/iplayer/Projects/rust/guessing_game/src/main.rs
